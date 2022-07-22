f4da52edb03c2bc7392b2da37729260f28508cee
vicendue <vmoyano@duemint.com>
2022-07-22 12:06:09 -0400

    origin cors

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Habilitar CORS
-app.use(cors());
+app.use(cors({origin: '*'}));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
